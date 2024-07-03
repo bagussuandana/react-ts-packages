c5b8f7ce9f92f6a69e86973d9d205fabc3f519b4
Gusti Bagus Suandana <gusti@vinceredigital.com>
2024-07-03 18:44:58 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { BaseLayout } from "admin-panel-layout";
+import { Button } from "@MidoAhmed/rollup-react-library-starter";
+function App() {
+    return (
+        <div>
+            <BaseLayout nav={<div>nav</div>}>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus
+                veritatis, nesciunt veniam hic quae, architecto sapiente
+                corrupti officia alias pariatur sunt distinctio voluptate
+                molestiae dolores a id fuga aliquam! At, tempore consequatur
+                nostrum dicta quisquam modi aperiam assumenda exercitationem sed
+                ipsum consequuntur magni reiciendis, laborum asperiores quae
+                dolores cupiditate eos? Voluptatibus laborum quos ipsam iste
+                molestias aperiam assumenda magni minus saepe. Accusamus,
+                laborum est ex odio quis iste doloribus id voluptatum, maxime
+                incidunt possimus non, aperiam quibusdam quidem nam? Quod
+                adipisci quo quam, explicabo temporibus dolore quibusdam nam
+                labore voluptates commodi nisi rerum veniam at vero aliquam
+                velit nihil ullam.
+            </BaseLayout>
+            <div>
+                <Button label="test" />
+            </div>
+        </div>
+    );
+}
+
+export default App;
